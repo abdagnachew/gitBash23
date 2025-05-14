a1a6a3ab4fcd27cec47c369aadb85fcc03faf61d
Abe <abdagnachew@gmail.com>
2025-05-14 19:59:18 -0400

    2nd commit

--- src/main/java/class02.java
+++ src/main/java/class02.java
@@ -1,6 +1,11 @@
 public class class02 {
 
     public static void main(String[] args) {
-        
+
+
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
